2de3ae803f6ece4c06672520dbd96b64d8fa2abe
jRimbault <jacques.rimbault@gmail.com>
2022-03-25 11:57:00 +0100

    add a command line example toy program

---
+++ examples/cli.rs
@@ -0,0 +1,14 @@
+use rand::Rng;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let pattern = std::env::args().nth(1).expect("give me a regex pattern");
+    let n = std::env::args()
+        .nth(2)
+        .and_then(|arg| arg.parse().ok())
+        .unwrap_or(1);
+    let pattern = rand_regex::Regex::compile(&pattern, 1)?;
+    for result in rand::thread_rng().sample_iter::<String, _>(pattern).take(n) {
+        println!("{}", result);
+    }
+    Ok(())
+}
